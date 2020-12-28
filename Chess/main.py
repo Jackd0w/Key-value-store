027495315fb782fc7260fbf22838b989548cc037
Komarov Andrey <komarovandrey396@gmail.com>
2020-12-28 11:09:47 +0300

    Вгтпущт ьфыеук

--- Chess/main.py
+++ Chess/main.py
@@ -4,3 +4,10 @@ Create main functions for project
 '''
 
 
+import os
+import tempfile
+
+storage_path = os.path.join(tempfile.gettempdir(), 'storage data')
+with open (storage_path, 'w') as f:
+    
+ 
