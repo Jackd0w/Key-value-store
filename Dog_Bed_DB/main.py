d8a74590f7128e033c13509d4221548c290b1b3a
Komarov Andrey <komarovandrey396@gmail.com>
2020-12-28 14:12:31 +0300

    Добавил методы для чтения и записи

--- Dog_Bed_DB/main.py
+++ Dog_Bed_DB/main.py
@@ -2,3 +2,25 @@
 Key-value storage
 version 1.0.0
 '''
+
+import argparse
+import json
+import os
+import tempfile
+
+def read_data(storage_path):
+    # Прерываем, если пути не существует
+    if not os.path.exists(storage_path):
+        return {}
+    # Открываем на чтение    
+    with open(storage_path, 'r') as file:
+        raw_data = file.read()
+        if raw_data:
+            return json.loads(raw_data)
+        return {}
+
+def write_data(storage_path, data):
+    with open(storage_path, 'w') as f:
+        f.write(json.dumps(data))
+
+
